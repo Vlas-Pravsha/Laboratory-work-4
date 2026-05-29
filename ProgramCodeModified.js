9fe9b4ad95564ed8161b24ef2f4567c53d4ccd10
Vlas <112288735+Vlas-Pravsha@users.noreply.github.com>
2026-05-30 01:21:41 +0300

    Create ProgramCodeModified.js

---
+++ ProgramCodeModified.js
@@ -0,0 +1,9 @@
+var productName="RestaurantDemandBookingPlatform"
+var unusedVar=42
+
+function printProductName( name ){
+console.log( "Product name: " + name )
+var localUnused = 100
+}
+
+printProductName( productName )
